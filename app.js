67aeae843aab7f65a929bfbcec5114394b509722
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-09-06 12:35:12 -0400

    Moves all inquirer Qs into one questions module; updated functions in app.js accordingly.

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Yarrow017!ms",
+    password: "root1234",
     database: "EmployeeTracker"
 });
 
@@ -36,102 +36,47 @@ const askTask = () => {
 };
 
 const addToDb = () => {
-    inquirer.prompt([
-        {
-            type: 'list',
-            name: 'toAdd',
-            message: "What would you like to add?",
-            choices: [
-                "Employee",
-                "Role",
-                "Department"
-            ]
-        }
-    ]).then(answers => {
-        const toAdd = answers.toAdd;
-        if (toAdd === 'Employee') {
-            addEmployee();
-        } else if (toAdd === 'Role') {
-            addRole();
-        } else {
-            console.log('add dept');
-        }
-    });
+    const toAdd = answers.toAdd;
+    if (toAdd === 'Employee') {
+        addEmployee(answers);
+    } else if (toAdd === 'Role') {
+        addRole(answers);
+    } else {
+        console.log('add dept');
+    }
 };
 
-const addEmployee = () => {
-    inquirer.prompt([
+const addEmployee = (answers) => {
+    connection.query(
+        "INSERT INTO employees SET ?",
         {
-            type: 'input',
-            name: 'firstName',
-            message: "What is the employee's first name?"
+            first_name: answers.firstName,
+            last_name: answers.lastName,
+            role_id: Number(answers.roleId),
+            manager_id: Number(answers.managerId)
         },
-        {
-            type: 'input',
-            name: 'lastName',
-            message: "What is the employee's last name?"
-        },
-        {
-            type: 'input',
-            name: 'roleId',
-            message: "What is the employee's role id?"
-        },
-        {
-            type: 'input',
-            name: 'managerId',
-            message: "What is the id of the employee's manager? (leave blank if no manager)",
-            default: null
+        function(err) {
+            if (err) throw err;
+            console.log("Your employee was added successfully!");
+            // re-prompt the user for the next task (or to exit)
+            askTask();
         }
-    ]).then(answers => {
-        connection.query(
-            "INSERT INTO employee SET ?",
-            {
-              first_name: answers.firstName,
-              last_name: answers.lastName,
-              role_id: Number(answers.roleId),
-              manager_id: Number(answers.managerId)
-            },
-            function(err) {
-              if (err) throw err;
-              console.log("Your employee was added successfully!");
-              // re-prompt the user for if they want to bid or post
-              askTask();
-            }
-          );
-    })
+    );
 };
 
-const addRole = () => {
-    inquirer.prompt([
+const addRole = (answers) => {
+    connection.query(
+        "INSERT INTO role SET ?",
         {
-            type: 'input',
-            name: 'title',
-            message: "What is the title of the role?"
+            title: answers.title,
+            salary: answers.salary,
+            department_id: Number(answers.deptId),
         },
-        {
-            type: 'input',
-            name: 'salary',
-            message: "What is the salary for this role?"
-        },
-        {
-            type: 'input',
-            name: 'deptId',
-            message: "What is the department ID for this role?"
-        },
-    ]).then(answers => {
-        connection.query(
-            "INSERT INTO role SET ?",
-            {
-              title: answers.title,
-              salary: answers.salary,
-              department_id: Number(answers.deptId),
-            },
-            function(err) {
-              if (err) throw err;
-              console.log("The role was added successfully!");
-              // re-prompt the user for if they want to bid or post
-              askTask();
-            }
-          );
-    })
+        function(err) {
+            if (err) throw err;
+            console.log("The role was added successfully!");
+            // re-prompt the user for if they want to bid or post
+            askTask();
+        }
+        );
 }
--- questions.js
+++ questions.js
@@ -4,12 +4,82 @@ const questions = [
         name: 'task',
         message: 'What would you like to do with your database?',
         choices: [
-            "view information",
-            "add new information",
-            "update existing information",
-            "exit application"
+            'view information',
+            'add new information',
+            'update existing information',
+            'exit application'
         ]
-    }
+    },
+    // When 'add new information' is picked.
+    {
+        type: 'list',
+        name: 'toAdd',
+        message: "What would you like to add?",
+        choices: [
+            'Employee',
+            'Role',
+            'Department'
+        ],
+        when: answers.tasks === 'add new information'
+    },
+
+    // To ADD new EMPLOYEE:
+    {
+        type: 'input',
+        name: 'firstName',
+        message: "What is the employee's first name?",
+        when: answers.toAdd === 'Employee'
+    },
+    {
+        type: 'input',
+        name: 'lastName',
+        message: "What is the employee's last name?",
+        when: answers.toAdd === 'Employee'
+    },
+    {
+        type: 'input',
+        name: 'roleId',
+        message: "What is the employee's role id?",
+        when: answers.toAdd === 'Employee'
+    },
+    {
+        type: 'input',
+        name: 'managerId',
+        message: "What is the id of the employee's manager? (leave blank if no manager)",
+        default: null,
+        // Use options from Managers table
+        when: answers.toAdd === 'Employee',
+    },
+
+    // To ADD new ROLE:
+    {
+        type: 'input',
+        name: 'title',
+        message: "What is the title of the role?",
+        when: answers.toAdd === 'Role',
+    },
+    {
+        type: 'input',
+        name: 'salary',
+        message: "What is the salary for this role?",
+        when: answers.toAdd === 'Role',
+    },
+    {
+        type: 'input',
+        name: 'deptId',
+        message: "What is the department ID for this role?",
+        // Use options from departments table
+        when: answers.toAdd === 'Role',
+    },
+
+    // To add new DEPARTMENT
+    {
+        type: 'input',
+        name: 'deptName',
+        message: "What is the name of the department?",
+        when: answers.toAdd === 'Department',
+    },
+
 ]
 
 module.exports = questions;
