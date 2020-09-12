f328713bf8993552de8177b24cacf2a2658ba59a
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-09-11 23:31:14 -0400

    changed function names and removed unneeded variables.

--- getAll.js
+++ getAll.js
@@ -3,12 +3,7 @@ const connection = require('./config/db.js');
 // Async/await use From https://stackoverflow.com/questions/44004418/node-js-async-await-using-with-mysql#:~:text=if%20you%20happen%20to%20be,()%20with%20the%20node%20mysql.&text=Any%20promise%20can%20be%20used,an%20async%20function%20%22wrapper%22.
 const queryAsync = util.promisify(connection.query).bind(connection);
 
-let allEmployees = [];
-let allRoles = [];
-let allDepts = [];
-let allManagers = [];
-
-const setAllEmp = async (value) => {
+const getAllEmp = async (value) => {
     try {
         const rows = await queryAsync("SELECT * FROM employees");
         return rows.map((employee) => ({ name: `${employee.employee_firstname} ${employee.employee_lastname}`, value: employee.id}));
@@ -17,7 +12,7 @@ const setAllEmp = async (value) => {
     }
 };
 
-const setAllRoles = async () => {
+const getAllRoles = async () => {
     try {
         const rows = await queryAsync("SELECT * FROM roles");
         return rows.map((role) => ({name: role.title, value: role.id}));
@@ -26,7 +21,7 @@ const setAllRoles = async () => {
     }
 };
 
-const setAllDepts = async() => {
+const getAllDepts = async() => {
     try {
         const rows = await queryAsync("SELECT * FROM departments");
         return rows.map((dept) => ({name: dept.dept_name, value: dept.id}));
@@ -35,7 +30,7 @@ const setAllDepts = async() => {
     }
 };
 
-const setAllManagers = async () => {
+const getAllManagers = async () => {
     try {
         const rows = await queryAsync("SELECT * FROM employees WHERE manager_id IS NULL");
         return rows.map((manager) => ({name: `${manager.employee_firstname} ${manager.employee_lastname}`, value: manager.id}));
@@ -44,15 +39,15 @@ const setAllManagers = async () => {
     }
 };
 
-//Not sure this is even necessary.... put into other functions...
-Promise.all([setAllEmp(), setAllRoles(), setAllDepts(), setAllManagers()])
-.then((values) => {
-    allEmployees = values[0];
-    allRoles = values[1];
-    allDepts = values[2];
-    allManagers = values[3];
-   // console.log('All employees:', allEmployees, 'All roles:', allRoles, 'All depts:', allDepts, 'all managers:', allManagers);
-}).catch((err) => console.log(err));
+// //Not sure this is even necessary.... put into other functions...
+// Promise.all([setAllEmp(), setAllRoles(), setAllDepts(), setAllManagers()])
+// .then((values) => {
+//     allEmployees = values[0];
+//     allRoles = values[1];
+//     allDepts = values[2];
+//     allManagers = values[3];
+//    // console.log('All employees:', allEmployees, 'All roles:', allRoles, 'All depts:', allDepts, 'all managers:', allManagers);
+// }).catch((err) => console.log(err));
 
 
-module.exports = { setAllEmp, setAllRoles, setAllDepts, setAllManagers }
+module.exports = { getAllEmp, getAllRoles, getAllDepts, getAllManagers }
