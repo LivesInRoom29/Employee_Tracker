5b5a087e8f335f21c5b74e32211ed95cd2d01bf9
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-09-13 10:57:24 -0400

    Created DAL with view all functions, delete viewall.js

---
+++ controllers/dal.js
@@ -0,0 +1,31 @@
+const cTablle = require('console.table');
+const connection = require('../config/db.js');
+
+
+const dal = {
+    viewAll: function(query) {
+        return new Promise((resolve, reject) => {
+
+            connection.query(query, (err, result) => {
+                if (err) reject(err);
+                console.log('\n');
+                console.table(result);
+                console.log('\n');
+                resolve(result);
+            });
+        });
+    },
+    viewAllBy: function(query, colToSearch, valueOfCol) {
+        return new Promise((resolve, reject) => {
+            connection.query(query, [colToSearch, valueOfCol], (err, result) => {
+                if (err) reject(err);
+                console.log('\n');
+                console.table(result);
+                console.log('\n');
+                resolve(result);
+            })
+        })
+    }
+};
+
+module.exports = dal
