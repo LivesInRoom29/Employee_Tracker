014432d55ec7ee0758a1ead59e7443c2496dc88c
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-09-13 19:04:26 -0400

    Fixed comments, cleaned up code, removed console.log

--- controllers/viewByMng.js
+++ controllers/viewByMng.js
@@ -1,25 +1,22 @@
 const inquirer = require('inquirer');
-
 const { getAllManagers } = require('./getAll');
-console.log(getAllManagers);
 
-// Add a new employee to the db
-// Instead of requiring askTask above, use it as a parameter. Otherwise it was being imported before it was actually being exported from the module.
+// Use inquirer to ask for which manager the user wants to see the employees.
 const viewByMng = () => {
     return new Promise ((resolve, reject) => {
-        // Call these 2 asyn functions to get an array of all the roles and all the managers
-        // These will be used to populate the choices for the 3rd and 5th questions.
-        // Promise.all([ getAllManagers()])
+        // 1st async function will get an array of objects for all the managers.
+        // These will be used to populate the choices for the question.
         getAllManagers()
         .then((allManagers) =>
             inquirer.prompt([
-            {
-                type: 'list',
-                name: 'managerId',
-                message: "For which manager would you like to see their employees?",
-                choices: allManagers
-            }
-        ]))
+                {
+                    type: 'list',
+                    name: 'managerId',
+                    message: "For which manager would you like to see their employees?",
+                    choices: allManagers
+                }
+            ])
+        )
         .then((answers) => resolve(answers))
         .catch((err) => reject(err))
     });
