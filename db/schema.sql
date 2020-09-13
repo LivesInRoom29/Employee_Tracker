e8cc476a9a3f13ae6dea729a3bf7b9fb06c7cf49
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-09-13 19:05:13 -0400

    changed column names for employee table

--- db/schema.sql
+++ db/schema.sql
@@ -5,8 +5,8 @@ USE employeeTracker_DB;
 
 CREATE TABLE employees(
 	id INT NOT NULL AUTO_INCREMENT,
-	employee_firstname VARCHAR(30) NOT NULL,
-	employee_lastname VARCHAR(30) NOT NULL,
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
 	role_id INT NOT NULL,
     manager_id INT NULL,
 	PRIMARY KEY (id)
